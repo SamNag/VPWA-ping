ea94cc39127565eef0a3c0cfb2bf4701e7011ca8
Samuel Nagy <sam16nag2002@gmail.com>
2024-10-03 10:12:42 +0200

    login design

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,15 +1,5 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-
-        <q-toolbar-title class="absolute-center sf-pro-700">
-          ping
-        </q-toolbar-title>
-
-      </q-toolbar>
-    </q-header>
-
     <q-page-container>
       <router-view />
     </q-page-container>
--- src/pages/SignPage.vue
+++ src/pages/SignPage.vue
@@ -1,21 +1,23 @@
 <template>
-  <q-page class="bg-dark window-height window-width row justify-center items-center">
+  <q-page class=" window-height window-width row justify-center items-center custom-bg">
     <div class="column">
       <div class="row">
       </div>
       <div class="row">
-        <q-card flat bordered class="q-pa-lg shadow-1">
+
+        <q-card bordered class="q-pa-lg q-card">
           <q-card-section>
             <q-form class="q-gutter-md">
-              <q-input square filled clearable v-model="email" type="email" label="email" />
-              <q-input square filled clearable v-model="password" type="password" label="password" />
+              <h5 class="text-dark sf-pro-700 cente" >Sign in </h5>
+              <q-input  dense clearable rounded standout v-model="email" type="email" label="email" />
+              <q-input dense clearable rounded standout v-model="password" type="password" label="password" />
             </q-form>
           </q-card-section>
           <q-card-actions class="q-px-md">
-            <q-btn unelevated color="light-green-7" size="lg" class="full-width" label="Login" />
+            <q-btn rounded dense color="primary" size="lg" class="full-width sf-pro-600 " label="Login" />
           </q-card-actions>
-          <q-card-section class="text-center q-pa-none">
-            <p class="text-grey-6">Not reigistered? Created an Account</p>
+          <q-card-section class="text-center q-pa-none sf-pro-400">
+            <p class="text-dark">Not reigistered? Created an Account</p>
           </q-card-section>
         </q-card>
       </div>
@@ -37,6 +39,18 @@ export default {
 
 <style>
 .q-card {
-  width: 360px;
+  min-width: 350px;
+  border-radius: 20px;
+  background-color: rgba(255, 255, 255, 0.5);
+  box-shadow: 0 0 10px #ffffff;
+  border: white 1px solid;
+
 }
+
+.custom-bg {
+  background-image: url('src/css/bg.png');
+  background-size: cover;
+  background-position: center;
+}
+
 </style>
